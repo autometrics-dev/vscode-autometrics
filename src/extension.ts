f375ea384b4dc495bea55f4abf355ce39ce9237e
Jacco Flenter <flenter@users.noreply.github.com>
2023-03-22 11:23:05 +0100

    Add test and github workflow

--- src/extension.ts
+++ src/extension.ts
@@ -3,29 +3,46 @@
 import * as vscode from "vscode";
 import { getContent } from "./content";
 
+/**
+ * Returns either a string or undefined if the document/position
+ * don't justify showing a tooltip
+ */
+export function getFunctionName(
+  document: vscode.TextDocument,
+  position: vscode.Position,
+): string | void {
+  const textLine = document.lineAt(position.line);
+
+  const functionRegex = /def\s*(?<name>[\dA-z]+)?\s*\(/;
+  const match = textLine.text.match(functionRegex);
+  const name = match?.groups?.name;
+  const decoratorRegex = /@autometrics/g;
+
+  if (
+    name
+    && position.line > 1
+    && decoratorRegex.test(document.lineAt(position.line - 1).text)
+  ) {
+    return name;
+  }
+}
+
+export const PythonHover = {
+  provideHover(document: vscode.TextDocument, position: vscode.Position) {
+    const name = getFunctionName(document, position);
+    if (name) {
+      return new vscode.Hover(getContent(name));
+    }
+
+    // eslint-disable-next-line unicorn/no-useless-undefined
+    return undefined;
+  },
+};
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate() {
-  vscode.languages.registerHoverProvider("python", {
-    provideHover(document, position) {
-      const textLine = document.lineAt(position.line);
-
-      const functionRegex = /def\s*(?<name>[\dA-z]+)?\s*\(/;
-      const match = textLine.text.match(functionRegex);
-      const name = match?.groups?.name;
-      const decoratorRegex = /@autometrics/g;
-      if (
-        name
-        && position.line > 1
-        && decoratorRegex.test(document.lineAt(position.line - 1).text)
-      ) {
-        return new vscode.Hover(getContent(name));
-      }
-
-      // eslint-disable-next-line unicorn/no-useless-undefined
-      return undefined;
-    },
-  });
+  vscode.languages.registerHoverProvider("python", PythonHover);
 }
 
 // This method is called when your extension is deactivated
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,53 @@ import * as assert from "node:assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
+import * as myExtension from "../../extension";
+
+async function createTestDocument() {
+  const document = await vscode.workspace.openTextDocument({
+    language: "python",
+  });
+
+  const editor = await vscode.window.showTextDocument(document);
+  const content = `from autometrics.autometrics import autometrics
+
+@autometrics
+def div_unhandled(num1,num2):
+  result = num1 / num2
+  return result 
+`;
+  const editBuilder = (textEdit: vscode.TextEditorEdit) => {
+    textEdit.insert(new vscode.Position(0, 0), String(content));
+  };
+
+  await editor.edit(editBuilder, {
+    undoStopBefore: true,
+    undoStopAfter: false,
+  });
+
+  return document;
+}
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
 
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  test("should return hover content when hovering over a function", async () => {
+    const document = await createTestDocument();
+
+    const line = 3;
+    const character = 5;
+    const position = new vscode.Position(line, character);
+    const hover = myExtension.PythonHover.provideHover(document, position);
+    assert.ok(hover, "Should not be undefined/falsy");
+  });
+
+  test("should not return hover content when hovering over the decorator", async () => {
+    const document = await createTestDocument();
+
+    const line = 2;
+    const character = 5;
+    const position = new vscode.Position(line, character);
+    const hover = myExtension.PythonHover.provideHover(document, position);
+    assert.strictEqual(hover, undefined, "Should be undefined");
   });
 });
