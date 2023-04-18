962e20570fc5aa597a3a66b97bd2babe16c315f8
Arend van Beelen jr <arendjr@gmail.com>
2023-04-18 21:57:33 +0800

    PR feedback

--- src/uniq.ts
+++ src/uniq.ts
@@ -1,5 +1,5 @@
 /**
- * Returns an array with all duplicate values removed.
+ * Returns a new array with all duplicate values removed.
  */
 export function uniq<T>(array: Array<T>): Array<T> {
   return [...new Set(array).values()];
