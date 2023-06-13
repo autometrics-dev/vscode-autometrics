ebb4837b332d261964aeb937205d22ea068c9d14
Jacco Flenter <flenter@users.noreply.github.com>
2023-06-13 15:18:41 +0200

    Format code

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,83 +1,83 @@
 import parseDuration from "parse-duration";
 import type { Timestamp } from "fiberplane-charts";
 import {
-	AbsoluteTimeRange,
-	FlexibleTimeRange,
-	RelativeTimeRange,
+  AbsoluteTimeRange,
+  FlexibleTimeRange,
+  RelativeTimeRange,
 } from "../types";
 
 export type DateParts = {
-	month: number;
-	date: number;
-	year: number;
-	timestamp: Timestamp;
+  month: number;
+  date: number;
+  year: number;
+  timestamp: Timestamp;
 };
 
 export function getDateParts(date: Date): DateParts {
-	return {
-		month: date.getMonth(),
-		date: date.getDate(),
-		year: date.getFullYear(),
-		timestamp: date.toISOString(),
-	};
+  return {
+    month: date.getMonth(),
+    date: date.getDate(),
+    year: date.getFullYear(),
+    timestamp: date.toISOString(),
+  };
 }
 
 export const timestampToDate = (timestamp: Timestamp): Date =>
-	new Date(timestamp);
+  new Date(timestamp);
 
 export const secondsToTimestamp = (seconds: number): Timestamp =>
-	new Date(seconds * 1000).toISOString();
+  new Date(seconds * 1000).toISOString();
 
 export const timestampToSeconds = (timestamp: Timestamp): number =>
-	new Date(timestamp).getTime() / 1000;
+  new Date(timestamp).getTime() / 1000;
 
 export const msToTimestamp = (ms: number): Timestamp =>
-	new Date(ms).toISOString();
+  new Date(ms).toISOString();
 
 export function createDefaultTimeRange(): FlexibleTimeRange {
-	return {
-		type: "relative",
-		from: "now-1 hour",
-		to: "now",
-	};
+  return {
+    type: "relative",
+    from: "now-1 hour",
+    to: "now",
+  };
 }
 
 export function relativeToAbsoluteTimeRange(
-	timeRange: RelativeTimeRange,
+  timeRange: RelativeTimeRange,
 ): AbsoluteTimeRange {
-	const fromDuration = getNowDuration(timeRange.from.trim().toLowerCase());
-	if (fromDuration === null) {
-		throw new Error(`Invalid from range: ${timeRange.from}`);
-	}
+  const fromDuration = getNowDuration(timeRange.from.trim().toLowerCase());
+  if (fromDuration === null) {
+    throw new Error(`Invalid from range: ${timeRange.from}`);
+  }
 
-	const toDuration = getNowDuration(timeRange.to.trim().toLowerCase());
-	if (toDuration === null) {
-		throw new Error(`Invalid to range: expected 'now', got: '${timeRange.to}'`);
-	}
+  const toDuration = getNowDuration(timeRange.to.trim().toLowerCase());
+  if (toDuration === null) {
+    throw new Error(`Invalid to range: expected 'now', got: '${timeRange.to}'`);
+  }
 
-	if (fromDuration > toDuration) {
-		throw new Error("End date is before start date");
-	}
+  if (fromDuration > toDuration) {
+    throw new Error("End date is before start date");
+  }
 
-	const now = Date.now();
-	return {
-		type: "absolute",
-		from: msToTimestamp(now + fromDuration),
-		to: msToTimestamp(now + toDuration),
-	};
+  const now = Date.now();
+  return {
+    type: "absolute",
+    from: msToTimestamp(now + fromDuration),
+    to: msToTimestamp(now + toDuration),
+  };
 }
 
 function parseDurationText(
-	from: string,
+  from: string,
 ): { duration: string; sign: "+" | "-" } | null {
-	const match = /^now\s?(?<sign>(-|\+))(?<duration>(.*?))$/.exec(from);
+  const match = /^now\s?(?<sign>(-|\+))(?<duration>(.*?))$/.exec(from);
 
-	if (!match?.groups?.duration) {
-		return null;
-	}
+  if (!match?.groups?.duration) {
+    return null;
+  }
 
-	const { duration, sign } = match.groups;
-	return { duration, sign };
+  const { duration, sign } = match.groups;
+  return { duration, sign };
 }
 
 /**
@@ -85,42 +85,42 @@ function parseDurationText(
  * and returning the value in milliseconds.
  */
 export function getNowDuration(from: string): number | null {
-	if (from === "now") {
-		return 0;
-	}
+  if (from === "now") {
+    return 0;
+  }
 
-	const { duration, sign } = parseDurationText(from);
-	const parsed = parseDuration(duration);
-	if (parsed == null) {
-		return null;
-	}
+  const { duration, sign } = parseDurationText(from);
+  const parsed = parseDuration(duration);
+  if (parsed == null) {
+    return null;
+  }
 
-	return sign === "+" ? parsed : -parsed;
+  return sign === "+" ? parsed : -parsed;
 }
 
 export function validateRelativeTo(to: string): boolean {
-	return to.trim().toLowerCase() === "now";
+  return to.trim().toLowerCase() === "now";
 }
 
 export function formatDuration(timeRange: FlexibleTimeRange) {
-	if (timeRange.type === "relative" && timeRange.to === "now") {
-		const { duration } = parseDurationText(timeRange.from.trim().toLowerCase());
-		if (duration) {
-			return `Last ${duration}`;
-		}
-	}
+  if (timeRange.type === "relative" && timeRange.to === "now") {
+    const { duration } = parseDurationText(timeRange.from.trim().toLowerCase());
+    if (duration) {
+      return `Last ${duration}`;
+    }
+  }
 
-	return `${timeRange.from} - ${timeRange.to}`;
+  return `${timeRange.from} - ${timeRange.to}`;
 }
 
 function pluralize(
-	value: number,
-	unit: string,
-	{ skipOne = false }: { skipOne: boolean },
+  value: number,
+  unit: string,
+  { skipOne = false }: { skipOne: boolean },
 ) {
-	if (value === 1 && skipOne) {
-		return unit;
-	}
+  if (value === 1 && skipOne) {
+    return unit;
+  }
 
-	return `${value} ${unit}${value > 1 ? "s" : ""}`;
+  return `${value} ${unit}${value > 1 ? "s" : ""}`;
 }
