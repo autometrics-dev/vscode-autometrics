c46946d987c77664848f2c4f5d70787cffde2590
Jacco Flenter <flenter@users.noreply.github.com>
2023-06-22 16:46:28 +0200

    Improved rust support (#50)
    * Use updated env var name for disabling docs
    
    * Check if rust-analyzer extension is enabled

--- src/languages/rust/rust.ts
+++ src/languages/rust/rust.ts
@@ -4,15 +4,68 @@ import { createFunctionHover } from "../../functionHover";
 
 // TODO: See https://github.com/autometrics-dev/vscode-autometrics/issues/27
 //       for tracking the Rust implementation.
-export function activateRustSupport() {
+export async function activateRustSupport(context: vscode.ExtensionContext) {
+  vscode.languages.registerHoverProvider("rust", RustHover);
+
+  if (isRustAnalyzerInstalled()) {
+    // Next: disable the rust-analyzer docs by setting an environment variable.
+    await updateAutometricsDocsSetting(context);
+  } else {
+    vscode.window.showErrorMessage(
+      "Rust-analyzer extension is not installed or not active. Please install the extension and try again.",
+    );
+    context.subscriptions.push(
+      vscode.extensions.onDidChange(() => {
+        if (isRustAnalyzerInstalled()) {
+          updateAutometricsDocsSetting(context);
+        }
+      }),
+    );
+  }
+}
+
+function isRustAnalyzerInstalled() {
+  const hasExtension = vscode.extensions.getExtension(
+    "rust-lang.rust-analyzer",
+  );
+
+  return !!hasExtension;
+}
+
+async function updateAutometricsDocsSetting(context: vscode.ExtensionContext) {
   const rustAnalyzerConfig = vscode.workspace.getConfiguration("rust-analyzer");
-  if (rustAnalyzerConfig.server?.extraEnv?.AUTOMETRICS_DOCS_GEN !== "0") {
-    // Note: Name of the environment variable may still change.
-    // See: https://github.com/autometrics-dev/autometrics-rs/issues/80
-    rustAnalyzerConfig.update("server.extraEnv.AUTOMETRICS_DOCS_GEN", "0");
+
+  // But first, check if we've already done this before
+  // and if so, don't set it again. People may have changed the config on purpose
+  // and the extension should respect that.
+  const rustConfigChecked = context.workspaceState.get("rustConfigChecked");
+  if (rustConfigChecked === "true") {
+    return;
   }
 
-  vscode.languages.registerHoverProvider("rust", RustHover);
+  const extraEnv = rustAnalyzerConfig.server?.extraEnv || {};
+  if (extraEnv.AUTOMETRICS_DISABLE_DOCS !== "1") {
+    // The extension will (temporarily) set the rust-analyzer to reload on config change
+    const initialRestartValue = rustAnalyzerConfig.restartServerOnConfigChange;
+    if (!initialRestartValue) {
+      await rustAnalyzerConfig.update("restartServerOnConfigChange", true);
+    }
+
+    await rustAnalyzerConfig.update("server.extraEnv", {
+      ...extraEnv,
+      AUTOMETRICS_DISABLE_DOCS: "1",
+    });
+
+    // Restore the value if it was not originally set to true
+    if (initialRestartValue !== true) {
+      await rustAnalyzerConfig.update(
+        "restartServerOnConfigChange",
+        initialRestartValue,
+      );
+    }
+  }
+
+  context.workspaceState.update("rustConfigChecked", true);
 }
 
 export const RustHover = {
